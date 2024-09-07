7304c81e556c6d349cbb3a6ed209b8c7655ab181
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-09-07 17:38:38 -0500

    Add Eureka Server and Config Server full

---
+++ eureka-server/src/test/java/com/jorge/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
