6ca340bd4f0a0c427e2963071108989059f045ca
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-09-09 12:30:55 -0500

    Change Gateway routing from yml to java code

--- gatewayserver/src/main/java/com/jorge/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/jorge/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,9 @@ package com.jorge.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +13,16 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/api/inventory/**")
+						.uri("lb://INVENTORY")
+				)
+				.route(p -> p
+						.path("/api/orders/**")
+						.uri("lb://ORDERS")
+				).build();
+	}
 }
