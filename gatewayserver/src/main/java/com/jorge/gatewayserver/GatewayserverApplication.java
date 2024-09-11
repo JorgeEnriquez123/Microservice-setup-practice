aac973784635bde781fc058675180015bca2701f
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-09-10 20:38:30 -0500

    Add RedisRateLimiter in Gateway

--- gatewayserver/src/main/java/com/jorge/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/jorge/gatewayserver/GatewayserverApplication.java
@@ -1,17 +1,25 @@
 package com.jorge.gatewayserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @SpringBootApplication
 public class GatewayserverApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(GatewayserverApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
@@ -29,6 +37,8 @@ public class GatewayserverApplication {
 								.retry(retryConfig -> retryConfig.setRetries(3)
 										.setMethods(HttpMethod.GET)
 										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(resolver()))
 						)
 						.uri("lb://INVENTORY")
 				)
@@ -38,5 +48,17 @@ public class GatewayserverApplication {
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 						.uri("lb://ORDERS")
 				).build();
+	}
+
+	@Bean
+	KeyResolver resolver(){
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("X-Forwarded-For"))
+					.defaultIfEmpty("unknown")
+				.doOnNext(ip -> log.info("Ip Address: {}", ip));
+	}
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter(){
+		return new RedisRateLimiter(5, 10, 1);
 	}
 }
